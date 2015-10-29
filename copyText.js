32642de72f0c379258afdea9de381e681681d864
Thomas Hallock <thomas@1stdibs.com>
2015-10-29 19:42:46 -0400

    here is the code

---
+++ copyText.js
@@ -0,0 +1,45 @@
+"use strict";
+var serverVars = require('server-vars');
+var _ = require('underscore');
+// TODO: use a paths-approach to make it easier to search non-existant objects safely
+var svCopyPaths = [];
+var CopyText = function (options) {
+    options = _.extend({
+        copy: {}
+    }, options);
+    this._copy = options.copy;
+    return this;
+};
+CopyText.prototype = {
+    get: function (copyKey, options) {
+        var text;
+        options = _.extend({
+            passthrough: true
+        }, options);
+        text = this._copy[copyKey];
+        if (undefined === text) {
+            text = _.reduceRight(svCopyPaths, function (text, keyPrefix) {
+                return text || serverVars.get(keyPrefix + '.' + copyKey);
+            }, text) || options.passthrough && copyKey;
+        }
+        if (options.obj) {
+            text = _.template(text)(options.obj);
+        }
+        return text;
+    },
+    object: function () {
+        return this._copy;
+    },
+    extend: function (morecopy) {
+        return new CopyText({copy: _.extend({}, this._copy, morecopy)});
+    }
+};
+
+module.exports = function () {
+    return new CopyText();
+};
+module.exports.addGlobalSVPath = function (svCopyPath) {
+    svCopyPaths.push(svCopyPath);
+    // console.log(svCopyPaths)
+    return module.exports;
+};
---
+++ test/copyText-spec.js
@@ -0,0 +1,47 @@
+"use strict";
+var copyTextApi = require('../copyText');
+var serverVars = require('server-vars');
+    describe('copyText', function () {
+    var copyText;
+    beforeEach(function () {
+        copyText = copyTextApi();
+        serverVars._api.jasmineFakeGet({
+            test: {
+                nextLevel: {
+                    copy: {
+                        someKeyInSvCopy: 'next level copy'
+                    }
+                },
+                copy: {
+                    someKeyInSvCopy: 'foo bar foo'
+                }
+            }
+        });
+    });
+    it('should default to serverVars paths passed through addGlobalSVPath', function () {
+        copyTextApi.addGlobalSVPath('test.copy');
+        copyText = copyText.extend({'someKeyThatArrivedThroughExtend': 'bar foo baz'});
+        expect(copyText.get('someKeyInSvCopy')).toEqual('foo bar foo');
+        expect(copyText.get('someKeyThatArrivedThroughExtend')).toEqual('bar foo baz');
+    });
+    it('should render using a template object when options.obj is specified', function () {
+        copyText = copyText.extend({fooBar: 'this <%= obj.thing %> is a template'});
+        expect(copyText.get('fooBar', {obj: {thing: 'template'}})).toEqual('this template is a template');
+    });
+    it('should prefer the last path added by addGlobalSVPath', function () {
+        copyTextApi.addGlobalSVPath('test.nextLevel');
+        expect(copyText.get('copy.someKeyInSvCopy')).toEqual('next level copy');
+        copyTextApi.addGlobalSVPath('test');
+        expect(copyText.get('copy.someKeyInSvCopy')).toEqual('foo bar foo');
+    });
+    describe('extend', function () {
+        it('should an extended copy of the original', function () {
+            copyText = copyText.extend({foo: "hello"});
+            expect(copyText.get('foo')).toEqual('hello');
+            expect(copyText.get('bar')).toEqual('bar');
+            copyText = copyText.extend({bar: "world"});
+            expect(copyText.get('foo')).toEqual('hello');
+            expect(copyText.get('bar')).toEqual('world');
+        });
+    });
+});
